fda162961df7b5a9feec9cf88b7cfb8c7028e70d
ardy <christian.ardy06@gmail.com>
2019-04-01 17:50:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/spplayer/Config.java
@@ -0,0 +1,8 @@
+package com.example.spplayer;
+
+import java.net.URL;
+
+public class Config {
+    public static final String CLIENT_ID = "P81pS7E4LB8vUEvQM98oMNsLe1wbYJqg";
+    public static final String API_URL = "http://api.soundcloud.com";
+}
